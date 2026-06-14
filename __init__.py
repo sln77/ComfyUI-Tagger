a330e882c58da5e325827e592a065d2aeb961ff2
sln77 <raze899922@gmail.com>
2026-06-14 16:41:51 +0300

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,19 @@
 from .camie_tagger import CamieTaggerNode
 from .pixai_tagger import PixAITagger
+from .taggerine_tagger import TaggerineTaggerNode
 from .tag_combiner import TagCombiner
 
 NODE_CLASS_MAPPINGS = {
     "CamieTaggerNode": CamieTaggerNode,
     "PixAITagger": PixAITagger,
+    "TaggerineTaggerNode": TaggerineTaggerNode,
     "TagCombiner": TagCombiner
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CamieTaggerNode": "Camie Tagger",
     "PixAITagger": "PixAI Tagger",
+    "TaggerineTaggerNode": "Taggerine Tagger",
     "TagCombiner": "Tag Combiner"
 }
 
